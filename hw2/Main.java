0498908f756465bee56efd26af50d889d4302ce1
NestZ <bobonono2009@windowslive.com>
2019-08-31 16:11:37 +0700

    hw2 success

--- hw2/Main.java
+++ hw2/Main.java
@@ -8,15 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        DynamicArray var1 = new DynamicArray(2);
-        for(int i = 0;i < 10;i++){
-            var1.pushBack(i*i + 1);
-        }
-        var1.printStructure();
-        System.out.println(var1.get(10));
-        var1.set(10,555);
-        var1.remove(10);
-        var1.printStructure();
     }
     
 }
